50f2b8f2679e94db5723b08e0813232ee0239d82
Dmaro7817 <shkrob.d@ya.ru>
2025-07-08 12:18:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 # Общий таймфрейм для всех модулей
 TIMEFRAME = "1h"
 
+TP_LEVELS = [2.0, 2.5, 5.0]  # Take Profit уровни в %
+TP_PERCENTAGES = [60, 20, 20]  # Проценты от позиции на каждом TP
+
+SL_PERCENT = 5.0  # Stop Loss в %
+USE_TRAILING_STOP = True  # Включить трейлинг стоп
+TRAILING_STOP_ACTIVATION_PERCENT = 2.0  # Процент движения в прибыль для активации трейлинг-стопа
+TRAILING_STOP_PERCENT = 3.5  # Шаг трейлинг-стопа в %
+
+#-----ИНДИКАТОРЫ-----
 # Периоды для трёх TEMA линий (можно менять)
 TEMA_PERIODS = [8, 14, 21]
 
@@ -14,7 +23,7 @@ CMO_PERIOD = 14
 
 
 
-# СТРАТЕГИИ
+#-----СТРАТЕГИИ-----
 
 TEMA_ADX_CMO_ENABLED = True
 
